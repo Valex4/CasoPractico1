fc29d840ca46c1adfe7044c712fca41b1f0e8636
YahirAlexander <yam77123@hotmail>
2022-05-27 10:55:08 -0500

    Initial commit

---
+++ src/com/upchiapas/CasoPractico1/Main.java
@@ -0,0 +1,97 @@
+package com.upchiapas.CasoPractico1;
+
+import com.upchiapas.CasoPractico1.ListaPersona;
+import com.upchiapas.CasoPractico1.Persona;
+import com.upchiapas.CasoPractico1.Vehiculo;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class
+Main {
+    private static ListaPersona personas = new ListaPersona(); //
+    private static Scanner teclado = new Scanner(System.in);
+    public static void main(String[] args) {
+        byte opcion;
+
+        //Menú de la aplicación
+        do {
+            System.out.println("UNIVERSIDAD POLITÉCNICA DE CHIAPAS");
+            System.out.println("ACCESO VEHICULAR");
+            System.out.println("1. Agregar personas");
+            System.out.println("2. Mostrar personas");
+            System.out.println("3. Mostrar personas con vehículos");
+            System.out.println("4. Agregar vehículos");
+            System.out.println("5. Buscar personas");
+            System.out.println("6. Salir");
+            System.out.print("Selecciona una opción:");
+            opcion = teclado.nextByte();
+            teclado.nextLine();
+            switch (opcion) {
+                case 1:
+                    addPersona();
+                    break;
+                case 2:
+                    personas.imprimirPersonas();
+                    break;
+                case 3:
+                    personas.imprimirPersonaWithVehiculo();
+                    break;
+                case 4:
+                    addVehiculo();
+                    break;
+                case 5:
+                    searchPersona();
+                    break;
+            }
+            System.out.print("Presione una tecla para continuar...");
+            teclado.nextLine();
+        }while (opcion <6);
+
+    }
+
+    public static void addPersona(){
+        System.out.print("Nombre: ");
+        if (personas.addPersona(new Persona(teclado.nextLine())))
+            System.out.println("Registro almacenado exitosamente");
+        else
+            System.out.println("¡Warning! El registro no se registró exitosamente");
+
+    }
+    public static void addVehiculo(){
+        byte numero;
+        String marca;
+        short modelo;
+        System.out.println("Lista de personas registradas");
+        personas.imprimirPersonas();
+        System.out.println("Selecciona el número de la persona, para agregar vehiculo");
+        System.out.print("Número:");
+        numero = teclado.nextByte();
+        teclado.nextLine();
+        System.out.println("Datos del vehiculo");
+        System.out.print("Marca:");
+        marca = teclado.nextLine();
+        System.out.print("Modelo:");
+        modelo = teclado.nextShort();
+        if (personas.get(--numero).addVehiculo(new Vehiculo(marca,modelo)))
+            System.out.println("Registro almacenado exitosamente");
+        else
+            System.out.println("¡Warning! El registro no se registró exitosamente");
+        teclado.nextLine();
+    }
+
+    public static void searchPersona(){
+        String nombre;
+        Persona persona;
+        System.out.println("Módulo para buscar persona");
+        System.out.print("Indica el nombre a buscar:");
+        nombre = teclado.nextLine();
+        persona = personas.searchPersona(nombre);
+        if (persona != null)
+            System.out.println(persona);
+        else System.out.println("No existe la persona buscada");
+    }
+
+    public Main(){}
+}
+
